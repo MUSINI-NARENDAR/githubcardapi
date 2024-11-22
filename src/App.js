d6a496f554f663d8b2f1298b2eab413aeae007c5
MUSINI-NARENDAR <147725200+MUSINI-NARENDAR@users.noreply.github.com>
2024-11-22 11:51:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React , { Component } from 'react';
+import GithubCard from './GithubCard';
 
-function App() {
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+        username :'',
+    };
+
+  }
+  // to update the username
+  handleInputChange = (event) =>{
+    this.setState({ username: event.target.value });
+  };
+
+  handleFormSubmit = (event) =>{
+    event.preventDefault();
+
+  };
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Github Profile</h1>
+      <form onSubmit={this.handleFormSubmit}>
+        <input type='text'
+        placeholder='enter username'
+        value={this.state.username}
+        onChange={this.handleInputChange}
+        />
+        <button type='submit'>Search</button>
+
+      </form>
+      {
+        this.state.username && (
+          <GithubCard username={this.state.username}/>
+        )
+      }
+      
+    
     </div>
   );
+}
 }
 
 export default App;
